4b8202d6f1c3d6bfd5f8d071b20f31ac4fd67b3b
MichaelZett <michael2.zoeller@gmail.com>
2023-12-20 16:07:02 +0100

    Clean up

--- src/main/java/de/zettsystems/exercise/StudentManagement.java
+++ src/main/java/de/zettsystems/exercise/StudentManagement.java
@@ -1,9 +1,7 @@
 package de.zettsystems.exercise;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
